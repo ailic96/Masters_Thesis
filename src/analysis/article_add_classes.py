c2665c9df1ba88f7c624de5b4f96b444101b9a12
Anton <ailic666@gmail.com>
2021-05-12 14:23:00 +0200

    Cosmetical changes to lemmatization, new column descriptions in add_classes

--- src/analysis/article_add_classes.py
+++ src/analysis/article_add_classes.py
@@ -10,13 +10,25 @@ def assign_classes(path):
     desc_title_positivity = df['Title_positivity'].describe()
     desc_subtitle_positivity = df['Subtitle_positivity'].describe()
     desc_text_positivity = df['Text_positivity'].describe()
+
+    desc_title_positivity_sentence = df['Title_positivity_sentence'].describe()
+    desc_subtitle_positivity_sentence  = df['Subtitle_positivity_sentence'].describe()
+    desc_text_positivity_sentence  = df['Text_positivity_sentence'].describe()
+
     desc_emoji_positivity = df['Emoji_positivity'].describe()
 
     print('Full stats:\n', desc_df)
     print('Title positivity:\n', desc_title_positivity)
     print('Subtitle positivity:\n', desc_subtitle_positivity)
     print('Article text positivity:\n', desc_text_positivity)
+    print('Title positivity (Sentence):\n', desc_title_positivity_sentence)
+    print('Subtitle positivity (Sentence):\n', desc_subtitle_positivity_sentence)
+    print('Article text positivity (Sentence):\n', desc_text_positivity_sentence)
+
     print('Emoji positivity:\n', desc_emoji_positivity)
+
+
+
 
 
     conditions = [
